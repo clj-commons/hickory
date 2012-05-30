bd2715b486abc56f2c8ac8b9876a341ab54fcad9
David Santiago <david.santiago@gmail.com>
2012-05-30 00:59:12 -0700

    Update dependency to Jsoup 1.6.3.

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.jsoup/jsoup "1.6.2"]])
+                 [org.jsoup/jsoup "1.6.3"]])
