78eadf00d04227eb9c75d7e7204de03c56fec706
David Santiago <david.santiago@gmail.com>
2013-05-21 21:20:12 -0700

    Add codox support for doc generation.

--- project.clj
+++ project.clj
@@ -5,4 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [quoin "0.1.0"]
-                 [org.jsoup/jsoup "1.7.1"]])
+                 [org.jsoup/jsoup "1.7.1"]]
+  :plugins [[codox "0.6.4"]]
+
+  :codox {:output-dir "codox-out"
+          :src-dir-uri "http://github.com/davidsantiago/hickory/blob/master"
+          :src-linenum-anchor-prefix "L"})
+
