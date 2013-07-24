dea2fac24a583d9ea9bb02f91fd13d0111f5a111
David Santiago <david.santiago@gmail.com>
2013-07-23 23:55:59 -0700

    Implement hiccup-fragment-to-hickory and hickory-to-hiccup, with tests.

--- src/hickory/convert.clj
+++ src/hickory/convert.clj
@@ -1,15 +1,57 @@
 (ns hickory.convert
   "Functions to convert from one representation to another."
   (:require [hickory.render :as render]
-            [hickory.core :as core]))
+            [hickory.core :as core]
+            [hickory.utils :as utils]
+            [quoin.text :as qt]))
 
 (defn hiccup-to-hickory
-  "Given a sequence of hiccup forms representing a document,
-   returns an equivalent hickory representation. It will parse
-   as a complete HTML document.
+  "Given a sequence of hiccup forms representing a full document,
+   returns an equivalent hickory node representation of that document.
+   This will perform HTML5 parsing as a full document, no matter what
+   it is given.
 
    Note that this function is heavyweight: it requires a full HTML
    re-parse to work."
   [hiccup-forms]
   (core/as-hickory (core/parse (render/hiccup-to-html hiccup-forms))))
+
+(defn hiccup-fragment-to-hickory
+  "Given a sequence of hiccup forms representing a document fragment,
+   returns an equivalent sequence of hickory fragments.
+
+   Note that this function is heavyweight: it requires a full HTML
+   re-parse to work."
+  [hiccup-forms]
+  (map core/as-hickory
+       (core/parse-fragment (render/hiccup-to-html hiccup-forms))))
+
+(defn hickory-to-hiccup
+  "Given a hickory format dom object, returns an equivalent hiccup
+   representation. This can be done directly and exactly, but in general
+   you will not be able to go back from the hiccup."
+  [dom]
+  (if (string? dom)
+    (qt/html-escape dom)
+    (try
+      (case (:type dom)
+        :document
+        (mapv hickory-to-hiccup (:content dom))
+        :document-type
+        (utils/render-doctype (get-in dom [:attrs :name])
+                              (get-in dom [:attrs :publicid])
+                              (get-in dom [:attrs :systemid]))
+        :element
+        (if (utils/unescapable-content (:tag dom))
+          (if (every? string? (:content dom))
+            ;; Merge :attrs contents with {} to prevent nil from getting into
+            ;; the hiccup forms when there are no attributes.
+            (apply vector (:tag dom) (into {} (:attrs dom)) (:content dom))
+            (throw (ex-info
+                    "An unescapable content tag had non-string children."
+                    {:error-location dom})))
+          (apply vector (:tag dom) (into {} (:attrs dom))
+                 (map hickory-to-hiccup (:content dom))))
+        :comment
+        (str "<!--" (apply str (:content dom)) "-->")))))
 
--- test/hickory/test/convert.clj
+++ test/hickory/test/convert.clj
@@ -6,10 +6,34 @@
 (deftest hiccup-to-hickory-test
   (is (= (as-hickory (parse "<i>Hi.</i>"))
          (hiccup-to-hickory (as-hiccup (parse "<i>Hi.</i>")))))
-  (is (= (as-hickory (parse "<i>Outer<b>Inner.</b></i>"))
-         (hiccup-to-hickory (as-hiccup (parse "<i>Outer<b>Inner.</b></i>")))))
+  (is (= (as-hickory (parse "<i>Outer<b class=\"foo\">Inner.</b></i>"))
+         (hiccup-to-hickory (as-hiccup (parse "<i>Outer<b class=\"foo\">Inner.</b></i>")))))
   (is (= (as-hickory (parse "<a href='http://localhost/?a=1&amp;b=2'>Hi</a>"))
          (hiccup-to-hickory (as-hiccup (parse "<a href='http://localhost/?a=1&amp;b=2'>Hi</a>")))))
   (is (= (as-hickory (parse "<script>alert();</script>"))
          (hiccup-to-hickory (as-hiccup (parse "<script>alert();</script>"))))))
+
+(deftest hiccup-fragment-to-hickory-test
+  (is (= (map as-hickory (parse-fragment "<img src=\"a.jpg\">"))
+         (hiccup-fragment-to-hickory (map as-hiccup (parse-fragment "<img src=\"a.jpg\">")))))
+  (let [src "<a href=\"/a.txt\"><img src=\"a.jpg\"></a><b>It's an a.</b>"]
+    (is (= (map as-hickory (parse-fragment src))
+           (hiccup-fragment-to-hickory (map as-hiccup (parse-fragment src)))))))
+
+
+(deftest hickory-to-hiccup-test
+  (is (= (as-hiccup (parse "<i>Hi.</i>"))
+         (hickory-to-hiccup (as-hickory (parse "<i>Hi.</i>")))))
+  (is (= (as-hiccup (parse "<i>Outer<b class=\"foo\">Inner.</b></i>"))
+         (hickory-to-hiccup (as-hickory (parse "<i>Outer<b class=\"foo\">Inner.</b></i>")))))
+  (is (= (as-hiccup (parse "<a href='http://localhost/?a=1&amp;b=2'>Hi</a>"))
+         (hickory-to-hiccup (as-hickory (parse "<a href='http://localhost/?a=1&amp;b=2'>Hi</a>")))))
+  (is (= (as-hiccup (parse "<script>alert();</script>"))
+         (hickory-to-hiccup (as-hickory (parse "<script>alert();</script>")))))
+  ;; Fragments
+  (is (= (map as-hiccup (parse-fragment "<img src=\"a.jpg\">"))
+         (map hickory-to-hiccup (map as-hickory (parse-fragment "<img src=\"a.jpg\">")))))
+  (let [src "<a href=\"/a.txt\"><img src=\"a.jpg\"></a><b>It's an a.</b>"]
+    (is (= (map as-hiccup (parse-fragment src))
+           (map hickory-to-hiccup (map as-hickory (parse-fragment src)))))))
 
