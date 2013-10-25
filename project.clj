92923a58a80d658e009ce91340b9df87f4b69899
David Santiago <david.santiago@gmail.com>
2013-10-25 02:21:49 -0700

    Update project version for further development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hickory "0.5.2"
+(defproject hickory "0.5.3-SNAPSHOT"
   :description "HTML as Data"
   :url "http://github.com/davidsantiago/hickory"
   :license {:name "Eclipse Public License"
