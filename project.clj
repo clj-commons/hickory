e12c6cafc1f1a667aa1e2b9d891580dc8d9df838
Erik Assum <erik.assum@ardoq.com>
2022-11-20 11:11:21 +0100

    Upgrade didn't go well

--- project.clj
+++ project.clj
@@ -10,9 +10,9 @@
                                     :sign-releases true}]]
 
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293" :scope "provided"]
                  [org.jsoup/jsoup "1.14.3"]
                  [quoin "0.1.2" :exclusions [org.clojure/clojure]]
-                 [org.clojure/clojurescript "1.9.293" :scope "provided"]
                  [viebel/codox-klipse-theme "0.0.1" :scope "provided"]]
 
   :hooks [leiningen.cljsbuild]
