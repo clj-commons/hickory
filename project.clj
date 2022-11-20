22eb606b51fe1fd9850e9ebda249a2df638d162d
Erik Assum <erik.assum@ardoq.com>
2022-11-20 11:18:37 +0100

    Upgrade lein-doo

--- project.clj
+++ project.clj
@@ -27,11 +27,11 @@
 
   :profiles
   {:dev  {:source-paths ["src/clj" "src/cljc"]
-          :dependencies [[lein-doo "0.1.7"]]
+          :dependencies [[lein-doo "0.1.11"]]
           :plugins      [[lein-cljsbuild "1.1.4"]
                          [lein-doo "0.1.7" :exclusions [org.clojure/clojure]]]}
    :test {:source-paths ["src/cljs" "src/cljc" "test/cljc" "test/cljs"]
-          :plugins      [[lein-doo "0.1.7" :exclusions [org.clojure/clojure]]]
+          :plugins      [[lein-doo "0.1.11" :exclusions [org.clojure/clojure]]]
           :cljsbuild    {:builds {:test {:source-paths  ["src/cljs" "src/cljc" "test/cljc" "test/cljs"]
                                          :compiler      {:output-to "target/cljs/testable.js"
                                                          :main      "hickory.doo-runner"}
